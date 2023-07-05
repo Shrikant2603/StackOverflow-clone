ad6c8f3a8ec1c9a5026a62a0081587427c372378
Shrikant2603 <93378209+Shrikant2603@users.noreply.github.com>
2023-07-05 17:45:50 +0530

    Added a chatbot

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,13 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import Reducers from "./reducers";
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 
+const appId = process.env.REACT_APP_KOMMUNICATE_APP_ID;
+Kommunicate.init(appId, {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true
+});
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(Reducers, composeEnhancers(applyMiddleware(thunk)));
 
